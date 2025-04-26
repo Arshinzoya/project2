a68518a77d797bcc3fb6d2425f468bcd05400635
Arshinzoya <ashupeerzade23@gmail.com>
2025-04-26 12:21:00 +0530

    Add files via upload

---
+++ ifelsestatement.java
@@ -0,0 +1,11 @@
+public class ifelsestatement{
+    public static void main(String args[]){
+        int num=12;
+        if(num>=0){
+            System.out.println("the number is positive");
+        }
+        else{
+            System.out.println("the number is negative");
+        }
+    }
+}
